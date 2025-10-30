281f05009924c187758772457cf805e0af5befe3
Kelvin Tetteh K. <tettehk537@gmail.com>
2025-10-30 17:13:00 +0000

    Login & Sign up page

--- School-Management-System-Frontend/src/pages/Home.jsx
+++ School-Management-System-Frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom"; 
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-blue-500'>This is the Home Page</h1>
+      <Link to="/auth">
+        <h3 className='cursor-pointer'>
+          Go to Auth Page
+        </h3>
+      </Link>
     </div>
   )
 }
